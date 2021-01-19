2a0a5451fe38ef007ee5f5b7522c54497814d3ad
CGonzalez97 <cgl76490@gmail.com>
2021-01-19 13:19:11 +0100

    conexion a mongo con exito

--- src/index.js
+++ src/index.js
@@ -14,8 +14,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 morganBody(app);
 
-app.listen(process.env.PORT, () =>
-  console.log(
-    `¡Aplicación de ejemplo escuchando en el puerto ${process.env.PORT}!`
-  )
-);
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+  
+  if (err) {
+    console.log(`Error de conexión a la base de datos: ${JSON.stringify(err)}`);
+  } else {
+    console.log(`Conexión correcta a la base de datos en la URI ${process.env.DB_URI}`);
+    app.listen(process.env.PORT, () =>
+      console.log(
+        `¡Aplicación de ejemplo escuchando en el puerto ${process.env.PORT}!`
+      )
+    );
+  }
+
+});
