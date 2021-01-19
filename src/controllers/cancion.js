1de025b391d268c31e7467c235d38d66eae42c68
CGonzalez97 <cgl76490@gmail.com>
2021-01-19 19:49:13 +0100

    Comentada una cosa

--- src/controllers/cancion.js
+++ src/controllers/cancion.js
@@ -3,11 +3,11 @@ import {Cancion,CancionRepo} from '../modelos/cancion';
 export const CancionController ={
 
     getCanciones: async (req,res) =>{
-        let cancionPrueba = new Cancion({title: 'Cancion1',
+        /*let cancionPrueba = new Cancion({title: 'Cancion1',
             artist: 'CarlitosBrown',
             album: 'API, calvicie y demás',
             year: new  Date()});
-        await CancionRepo.save(cancionPrueba);
+        await CancionRepo.save(cancionPrueba);*/
         let listaCanciones = await CancionRepo.findAll();
         if(listaCanciones != undefined || listaCanciones != null || listaCanciones.lenght <= 0){
             res.json(listaCanciones);//.sendStatus(200);
