b386a2bd878c8caef30c542a91cfe9de191d1d02
CGonzalez97 <cgl76490@gmail.com>
2021-01-22 09:26:45 +0100

    Validaciones añadidas, falta testearlas y más adelante se añadiran más si hacen falta

--- src/routes/cancion.js
+++ src/routes/cancion.js
@@ -1,14 +1,40 @@
 import { Router } from 'express';
+import { param, body } from 'express-validator';
 import {CancionController} from '../controllers/cancion';
+import {validar} from '../middlewares/validacion';
 
 const router = Router();
 
 
 router.get('/', CancionController.getCanciones);
-router.post('/', CancionController.crearCancion);
-router.get('/:id',CancionController.getCancion );
-router.put('/:id', CancionController.modCancion);
-router.delete('/:id', CancionController.deleteCancion);
+
+router.post('/',
+            [body('title').isString().withMessage('El título debe ser una cadena de caracteres.'),
+                  body('album').isString().withMessage('El título debe ser una cadena de caracteres.'),
+                  body('artist').isString().withMessage('El título debe ser una cadena de caracteres.'),
+                  body('year').isDate().withMessage('La fecha debe ser de tipo Fecha.')],
+            validar,
+            CancionController.crearCancion);
+
+
+router.get('/:id',
+            [param('id').isString().withMessage('El id debe ser una cadena de caracteres.')],
+            validar,
+            CancionController.getCancion );
+
+router.put('/:id',
+            [param('id').isString().withMessage('El id debe ser una cadena de caracteres.'),
+            body('title').isString().withMessage('El título debe ser una cadena de caracteres.'),
+            body('album').isString().withMessage('El título debe ser una cadena de caracteres.'),
+            body('artist').isString().withMessage('El título debe ser una cadena de caracteres.'),
+            body('year').isDate().withMessage('La fecha debe ser de tipo Fecha.')],
+            validar,
+            CancionController.modCancion);
+
+router.delete('/:id',
+            [param('id').isString().withMessage('El id debe ser una cadena de caracteres.')],
+            validar,
+            CancionController.deleteCancion);
 
 
 export default router;
--- src/routes/listaReproduccion.js
+++ src/routes/listaReproduccion.js
@@ -1,18 +1,57 @@
 import { Router } from 'express';
 import { ListaController} from '../controllers/listaReproduccion';
+import { Router } from 'express';
+import {param,body} from 'express-validator';
+import {validar} from '../middlewars/validacion';
 
 const router = Router();
 
-
 router.get('/', ListaController.getListas);
-router.post('/', ListaController.crearLista);
-router.get('/:id', ListaController.getLista);
-router.put('/:id', ListaController.modLista);
-router.delete('/:id', ListaController.deleteLista);
 
-router.get('/:id/songs', ListaController.getCanciones);
-router.post('/:id/songs/:idC', ListaController.addCancion);
-router.get('/:id/songs/:idC', ListaController.getCancion);
-router.delete('/:id/songs/:idC', ListaController.deleteCancion);
+router.post('/',
+    [body('name').isString().withMessage('El nombre de la lista debe ser una cadena decaracteres.'),
+    body('description').isString().withMessage('El descripción de la lista debe ser una cadena decaracteres.')],
+    validar,
+    ListaController.crearLista);
+
+router.get('/:id',
+    [param('id').isString().withMessage('El id debe de ser una cadena de caracteres.')],
+    validar,
+    ListaController.getLista);
+
+router.put('/:id',
+    [param('id').isString().withMessage('El id debe de ser una cadena de caracteres.'),
+    body('name').isString().withMessage('El nombre de la lista debe ser una cadena decaracteres.'),
+    body('description').isString().withMessage('El descripción de la lista debe ser una cadena decaracteres.')],
+    validar,
+    ListaController.modLista);
+
+router.delete('/:id',
+    [param('id').isString().withMessage('El id debe de ser una cadena de caracteres.')],
+    validar,
+    ListaController.deleteLista);
+
+router.get('/:id/songs',
+    [param('id').isString().withMessage('El id debe de ser una cadena de caracteres.')],
+    validar,
+    ListaController.getCanciones);
+
+router.post('/:id/songs/:idC',
+    [param('id').isString().withMessage('El id debe de ser una cadena de caracteres.'),
+    param('idC').isString().withMessage('El id debe de ser una cadena de caracteres.')],
+    validar,
+    ListaController.addCancion);
+
+router.get('/:id/songs/:idC',
+    [param('id').isString().withMessage('El id debe de ser una cadena de caracteres.'),
+    param('idC').isString().withMessage('El id debe de ser una cadena de caracteres.')],
+    validar,
+    ListaController.getCancion);
+
+router.delete('/:id/songs/:idC',
+    [param('id').isString().withMessage('El id debe de ser una cadena de caracteres.'),
+    param('idC').isString().withMessage('El id debe de ser una cadena de caracteres.')],
+    validar,
+    ListaController.deleteCancion);
 
 export default router;
